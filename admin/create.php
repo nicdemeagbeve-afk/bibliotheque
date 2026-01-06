52eb1b2a934bc560857244eac653195f6ea03153
AGBEVE Ayao Nicodème <nicdeme.agbeve@gmail.com>
2026-01-06 16:12:33 +0000

    Ajout de dernière minute

--- admin/create.php
+++ admin/create.php
@@ -97,7 +97,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" href="css/style.css">
+ <link rel="stylesheet" href="../css/style.css">
    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <title>Ajout de livre</title>
 </head>
--- admin/edit.php
+++ admin/edit.php
@@ -106,7 +106,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" href="css/style.css">
+ <link rel="stylesheet" href="../css/style.css">
    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <title>Modifier le livre</title>
 </head>
